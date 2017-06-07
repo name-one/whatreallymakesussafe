526fa4aab4747f826b45bcead5178b2ab7a78b3c
caaronbeasley <beasleycharles@gmail.com>
2017-06-07 11:34:34 +0200

    Update People.ts

--- src/app/model/People.ts
+++ src/app/model/People.ts
@@ -5,43 +5,43 @@ export const PEOPLE: Person[] = [
     id: 0,
     name: 'Melanie Brazzell',
     shortName: 'M',
-    color: '#FF031C',
+    color: '#D9ECD9',
     fontColor: 'white' },
   {
     id: 1,
     name: 'Dr. Alexis Pauline Gumbs',
     shortName: 'AP',
-    color: '#fe4a0f',
+    color: '#DDDAE9',
     fontColor: 'white' },
   {
     id: 2,
     name: 'Colin Atrophy Hagendorf',
     shortName: 'C',
-    color: '#ffb500',
+    color: '#E9D9EA',
     fontColor: 'white' },
   {
     id: 3,
     name: 'Dr. Sarah Tyson',
     shortName: 'S',
-    color: '#fdf001',
+    color: '#EADADE',
     fontColor: 'white' },
   {
     id: 4,
     name: 'Kiyomi Fujikawa',
     shortName: 'K',
-    color: '#c8dc00',
+    color: '#E7E9D9',
     fontColor: 'white' },
   {
     id: 5,
     name: 'Dr. Beth Richie',
     shortName: 'BR',
-    color: '#00a74b',
+    color: '#DAE9E9',
     fontColor: 'white' },
   {
     id: 6,
     name: 'Melissa Spatz',
     shortName: 'Me',
-    color: '#4accc0',
+    color: '#DAE5E9',
     fontColor: 'white' },
   {
     id: 7,
@@ -53,13 +53,13 @@ export const PEOPLE: Person[] = [
     id: 8,
     name: 'Maura M. Bairley',
     shortName: 'Ma',
-    color: '#554fa7',
+    color: '#00b0e1',
     fontColor: 'white' },
   {
     id: 9,
     name: 'Jenna Peters-Golden & Qui Alenxander',
     shortName: 'J & Q',
-    color: 'grey',
+    color: '#EADADA',
     fontColor: 'white' },
   {
     id: 10,
@@ -71,136 +71,136 @@ export const PEOPLE: Person[] = [
     id: 11,
     name: 'generationFIVE',
     shortName: 'gen5',
-    color: 'grey',
+    color: '#E9E5D9',
     fontColor: 'white' },
   {
     id: 12,
     name: 'Creative Interventions',
     shortName: 'CI',
-    color: 'grey',
+    color: '#E3E7D9',
     fontColor: 'white' },
   {
     id: 13,
     name: 'Challenging Male Supremacy Project',
     shortName: 'CMS',
-    color: 'grey',
+    color: 'E2DAE9',
     fontColor: 'white' },
   {
     id: 14,
     name: 'Support New York',
     shortName: 'nope',
-    color: 'grey',
+    color: '#E9DDD9',
     fontColor: 'white' },
   {
     id: 15,
     name: 'Norma',
     shortName: 'N',
-    color: '#c3046e',
+    color: '#E6E9D9',
     fontColor: 'white'
   },
   {
     id: 16,
     name: 'Alix Johnson',
     shortName: 'A',
-    color: '#777',
+    color: '#DAE9E2',
     fontColor: 'white'
   },
   {
     id: 17,
     name: 'Veronica',
     shortName: 'V',
-    color: '#777',
+    color: '#E5DAE9',
     fontColor: 'white'
   },
   {
     id: 18,
     name: 'Leah Todd and Lauren Spencer',
     shortName: 'L & L',
-    color: '#777',
+    color: '#DADAE9',
     fontColor: 'white'
   },
   {
     id: 19,
     name: 'Dr. Hediana Utarti',
     shortName: 'H',
-    color: '#777',
+    color: '#E9E9D9',
     fontColor: 'white'
   },
   {
     id: 20,
     name: 'Anna Vo',
     shortName: 'An',
-    color: '#777',
+    color: '#D9E9DC',
     fontColor: 'white'
   },
   {
     id: 21,
     name: 'Tod Augusta-Scott',
     shortName: 'T A-S',
-    color: '#777',
+    color: '#D9E9E0',
     fontColor: 'white'
   },
   {
     id: 22,
     name: 'Dr. Mimi Kim',
     shortName: 'MK',
-    color: '#777',
+    color: '#DADFE8',
     fontColor: 'white'
   },
   {
     id: 23,
     name: 'Dr. Erica Meiners',
     shortName: 'EM',
-    color: '#777',
+    color: '#D8E7DF',
     fontColor: 'white'
   },
   {
     id: 24,
     name: 'INCITE!',
     shortName: 'I!',
-    color: 'grey',
+    color: '#E8DAD9',
     fontColor: 'white'
   },
   {
     id: 25,
     name: 'FIERCE',
     shortName: 'FIERCE',
-    color: 'grey',
+    color: '#E7E7D9',
     fontColor: 'white'
   },
   {
     id: 26,
     name: 'Northwest Network',
     shortName: 'NW N',
-    color: 'grey',
+    color: '#E7E1D9',
     fontColor: 'white'
   },
   {
     id: 27,
     name: 'Harm Free Zones',
     shortName: 'HFZ',
-    color: 'grey',
+    color: '#D9E6E7',
     fontColor: 'white'
   },
   {
     id: 28,
     name: 'Communities Against Rape & Abuse',
     shortName: 'CARA',
-    color: 'grey',
+    color: '#DADAE5',
     fontColor: 'white'
   },
   {
     id: 29,
     name: 'Communities United Against Violence',
     shortName: 'CUAV',
-    color: 'grey',
+    color: '#D8E4D7',
     fontColor: 'white'
   },
   {
     id: 30,
     name: 'Bay Area Transformative Justice Collective',
     shortName: 'BATJC',
-    color: 'grey',
+    color: '#E4E0D7',
     fontColor: 'white'
   }
 ];
