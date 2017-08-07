653669ae7165d32c4829502a02fdc15b0f596e32
DenisKamensky <kengji45@gmail.com>
2017-08-07 17:15:21 +0300

    german page desctop layout

--- src/app/german/german.component.ts
+++ src/app/german/german.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-
+import { album, iAlbum } from '../model/album';
 @Component({
   selector: 'app-german',
   templateUrl: './german.component.html',
   styleUrls: ['./german.component.css'],
 })
 export class GermanComponent implements OnInit {
+  photos: iAlbum[];
+  photo: iAlbum;
   currentImg : string;
+  currentFrame: string = 'Podiumsdiskussion';
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.photos = album;
     document.body.classList.remove('nav-hidden')
   }
   openModal(content: any, src: string){
     this.currentImg = src;
+    const modalRef = this.modalService.open(content,
+      {backdrop: true,
+        keyboard: true,
+        size: 'lg',
+        windowClass: 'show'}).result.then((result) => {
+      console.log('Closed with: ${result}');
+    }, (reason) => {
+      this.currentImg = '';
+      console.log('Dismissed ${this.getDismissReason(reason)}');
+    });
+  }
+  modalPhoto(content: any, img: string, text: string){
+    console.log(content)
+    let newPhoto:iAlbum = {
+      src: img,
+      text,
+    }
+    this.photo = newPhoto;
     const modalRef = this.modalService.open(content,
       {backdrop: true,
         keyboard: true,
